8f870fbc342bee4547ebb8b5553653389c4d0af8
GuanYi-Start <1119132301@qq.com>
2020-03-16 17:53:00 +0800

    张三

---
+++ src/main/java/com/example/demoagi/DemoagiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoagi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoagiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoagiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demoagi/Test.java
@@ -0,0 +1,8 @@
+package com.example.demoagi;
+
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("00000000000");
+    }
+}
